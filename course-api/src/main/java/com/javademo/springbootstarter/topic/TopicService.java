f936eff57621b15c6550f0c5256c3a2ccd6f699f
Octavian <committer@example.com>
2020-03-24 18:26:44 +0200

    First try

---
+++ course-api/src/main/java/com/javademo/springbootstarter/topic/TopicService.java
@@ -0,0 +1,30 @@
+package com.javademo.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class TopicService {
+	
+	private List<Topic> topics = new ArrayList<>(Arrays.asList(
+ 			new Topic("spring", "Spring Framework", "Spring Framework description"),
+			new Topic("java", "Java Code", "Java Core description"),
+			new Topic("javascript", "JavaScript", "JavaScript description")
+			));
+	
+	public List<Topic> getAllTopics() {
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+		
+	}
+}
